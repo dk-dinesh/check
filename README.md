3615ad29a54c11281b575c2292bb9ebfbab0a4d9
Dinesh Kumar <55187704+dk-dinesh@users.noreply.github.com>
2020-06-02 01:29:33 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# GitCommitShow 2020 Homepage
+
+This repository contains the source code for the homepage of the gitcommitshow 2020 event.
+
+It uses the following stack as of now:
+
+1. HTML5   
+2. CSS3   
+3. Javascript   
+
+If you want to contribute to it, check out the [contributing guidelines](CONTRIBUTING.md)
---
+++ assets/js/slick-effect.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	$('.slider').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		arrows: true,
+		nextArrow: "<div class='slick-prev'><i class='fa fa-arrow-circle-right'></i></div>",
+		prevArrow: "<div class='slick-next'><i class='fa fa-arrow-circle-left'></i></div>",
+		centerMode: true,
+		centerPadding: '0',
+	});
+});
