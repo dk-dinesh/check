ad38a9ccd2b21047ace95428420d2aea1104882a
dinesh23kumar1998@gmail.com <dinesh23kumar1998@gmail.com>
2020-06-21 03:17:10 +0530

    new wepage gallery 2019 added

--- assets/js/count.js
+++ assets/js/count.js
@@ -1,4 +1,4 @@
-
+//countdown timer
 var second = 1000,
 minute = second * 60,
 hour = minute * 60,
@@ -14,6 +14,5 @@ document.getElementById('days').innerText = Math.floor(distance / (day)),
   document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
   document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
   document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-
 
 }, second)
--- assets/js/gallery-slick.js
+++ assets/js/gallery-slick.js
@@ -1,9 +1,16 @@
-$(document).ready(function () {
+     $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider'
+});
     $('.slider').slick({
         infinite: true,
         speed: 300,
         slidesToShow: 3,
         slidesToScroll: 1,
+        asNavFor: '.slider-for',
         arrows: true,
         prevArrow: "<img class='a-left control-c-left prev slick-prev' src='assets/img/left.svg '>",
         nextArrow:"<img class='a-right control-c-right next slick-next' src='assets/img/right.svg'>",
@@ -34,4 +41,3 @@ $(document).ready(function () {
 
   ]
     });
-});
